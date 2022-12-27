fd0291ab6a8983858db0358c5c756e63f500d246
Никита Иванов <13666nikita@gmail.com>
2022-12-27 19:06:11 +0300

    Спринт 7.27

--- homework.py
+++ homework.py
@@ -6,7 +6,6 @@ from logging import StreamHandler
 import requests
 import telegram
 from dotenv import load_dotenv
-from http import HTTPStatus
 
 import exceptions
 
@@ -15,10 +14,9 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-ENDPOINT = os.getenv('ENDPOINT')
 
 RETRY_PERIOD = 600
-
+ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
 
@@ -44,23 +42,23 @@ handler.setFormatter(formatter)
 
 def check_tokens():
     """Проверка что все токены есть."""
-    if all([TELEGRAM_TOKEN, PRACTICUM_TOKEN,
-            TELEGRAM_CHAT_ID, ENDPOINT]) is False:
-        logging.critical('Отсутствие обязательных переменных '
-                         'окружения во время запуска бота')
-        return False
-    else:
-        return True
+    massive = [TELEGRAM_TOKEN, PRACTICUM_TOKEN, TELEGRAM_CHAT_ID, ENDPOINT]
+    for i in massive:
+        if not i:
+            logging.critical('Отсутствие обязательных переменных '
+                             f'{i} окружения во время запуска бота ')
+            return False
+    return True
 
 
 def send_message(bot, message):
     """Отправка сообщения ботом."""
+    chat_id = TELEGRAM_CHAT_ID
     text = message
-    logging.info('Попытка отправки сообщения')
     try:
-        bot.send_message(TELEGRAM_CHAT_ID, text)
-        logging.debug(f'Отправил сообщения {text}')
-    except telegram.error.TelegramError as error:
+        logging.debug(f'Отправка сообщения {text}')
+        bot.send_message(chat_id, text)
+    except Exception as error:
         logging.error(f'Сбой в работе программы: {error}')
         raise exceptions.SendmessageError(f'Ошибка отправки сообщения{error}')
 
@@ -74,10 +72,10 @@ def get_api_answer(timestamp):
                                          params=payload)
     except requests.exceptions.RequestException as error:
         raise exceptions.PracticumAPIError(f'Ошибка запроса {error}')
-    if homework_statuses.status_code == HTTPStatus.BAD_REQUEST:
+    if homework_statuses.status_code == 400:
         logging.error('Недоступность эндпоинта')
         raise exceptions.PracticumAPIError('Api Yandex не работает')
-    if homework_statuses.status_code != HTTPStatus.OK:
+    if homework_statuses.status_code != 200:
         logging.error('сбои при запросе к эндпоинту ',
                       f'{homework_statuses.status_code}')
         raise exceptions.PracticumAPIError('Api запрос Yandex не проходит')
@@ -94,7 +92,7 @@ def check_response(response):
         value = response['homeworks']
     except KeyError:
         raise KeyError('Нет ключа в словаре')
-    if not isinstance(response['homeworks'], list):
+    if type(response['homeworks']) != list:
         raise TypeError('Не список')
     if 'code' in response:
         raise exceptions.PracticumAPIError('Ошибка ответа API сервера')
@@ -117,8 +115,6 @@ def parse_status(homework):
     if homework_status not in HOMEWORK_VERDICTS:
         logging.error('Неверный статус домашки')
         raise NameError('Неверный статус домашки')
-    if 'current_date' in homework:
-        raise Exception('Ошибка возврата времени')
 
     verdict = HOMEWORK_VERDICTS[homework_status]
 
@@ -129,7 +125,7 @@ def main():
     """Основная логика работы бота."""
     answer = ''
     if not check_tokens():
-        raise Exception('Ошибка токена')
+        raise exceptions.TokenError('Ошибка токена')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
 
